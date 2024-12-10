bc003cbf3c9c07e5453fd7a2040b755b772f2b30
Luiz Ricardo <luizricardo.lr278@gmail.com>
2024-12-09 21:59:45 -0300

    calculadoraIMC.java

--- src/calculadoraIMC.java
+++ src/calculadoraIMC.java
@@ -1,52 +1,59 @@
-//Escreva um programa que calcule o IMC de um indivíduo, utilizando a
-//fórmula IMC = peso / altura2
-
-
+package grupo;
 
 import java.util.Scanner;
 
-public class calculadoraIMC {
-  
-  public static void main(String[] args) {
-      
-      Scanner scanner = new Scanner(System.in);
+public class ExercicioIMC {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-      //teclado de qual tipo!
-      System.out.println("Digite seu nome:");
-      String nome =  scanner.nextLine();
-      System.out.println ("Digite sua idade:");
-      int  idade = scanner.nextInt();
-      System.out.println("Digite o peso em quilogramas:");
-      double peso  = scanner.nextDouble();
-      System.out.println("Digite a altura em metros:");
-      double altura = scanner.nextDouble();
+        System.out.println("Exercicio IMC - Calculadora de IMC" + "\n");
 
-      double imc  = peso / (altura * altura);
+        System.out.println("Digite o nome da pessoa: ");
+        String nome = sc.nextLine();
 
-      //mostrar é rescultado!
-      System.out.println("O IMC é: " + imc);
-      System.out.printf ("Seu nome: " + nome + "\n");
-      System.out.println("O Seu Idade é: " + idade );
+        System.out.println("Digite a idade da pessoa: ");
+        int idade = sc.nextInt();
 
+        System.out.println("Digite o peso da pessoa (kg): ");
+        double peso = sc.nextDouble();
 
-      //tipo Otimo, bem, mal, ruim
-      if (imc < 18.5)
-      System.out.println ("Abaixo do peso ideal, vamos melhorar!");
-      
-      else if ((imc > 18.5) &&(imc <24.9))
-          System.out.println ("Voce esta no peso ideal, parabens!");
+        System.out.println("Digite a altura da pessoa (m): ");
+        double altura = sc.nextDouble();
 
-      else if ((imc > 25.0)&& (imc <29.9))
-          System.out.println("Voce está com excesso de peso, vamos melhorar!");
-      else if ((imc > 30.0) && (imc <34.9))
-          System.out.println("Obesidade Classe I, cuidado!");
-      else if ((imc > 35.0) && (imc <39.9))
-          System.out.println ("Obesidade Classe II, ainda temos como mudar isso!");
-      else if (imc >= 40)
-          System.out.println ("Obesidade Classe III, procure ajuda, nada está perdido!");
-             
-        System.out.println("Obrigado :)");
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+    
+        double imc = calcularIMC(peso, altura);
 
-      scanner.close();
-  }
+        classificarPessoa(imc);
+
+        System.out.println("IMC: " + imc);
+        
+        sc.close();
+    }
+    
+    public static double calcularIMC(double peso, double altura) {
+        return peso / (altura * altura);
+
+    }
+    public static void classificarPessoa(double imc) {
+        String classificacao;
+
+        if (imc < 18.5) {
+            classificacao = "A pessoa está abaixo do peso.";
+        } else if (imc > 18.5 && imc < 25) {
+            classificacao = "A pessoa está no peso normal.";
+        } else if (imc > 25.0 && imc < 29.9) {
+            classificacao = "A pessoa está com sobrepeso.";
+        } else if (imc > 30.0 && imc < 34.9) {
+            classificacao = "A pessoa está com obesidade grau I.";
+        } else if (imc > 35.0 && imc < 39.9) {
+            classificacao = "A pessoa está com obesidade grau II.";
+        } else {
+            classificacao = "A pessoa está com obesidade grau III.";
+        }
+
+        System.out.println(classificacao);
+
+    }
 }
